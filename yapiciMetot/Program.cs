7c5e6ddbf567dade5ea8b1b13eba12929edbbe36
hasan566 <hasan566@BPC02>
2023-12-12 08:41:17 +0300

    Proje dosyası ekle.

---
+++ yapiciMetot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yapiciMetot
+{
+    class kisi
+    {
+        int yas;
+        string ad;
+        public kisi()
+        {
+            yas = 15;
+            ad = "LEVENT YANIK";
+            Console.WriteLine("Yapıcı metot çalıştı");
+        }
+        public int Yas
+        {
+            get
+            {
+                return yas;
+            }
+        }
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+        }
+    }
+        
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program başladı.");
+            kisi k = new kisi();
+            Console.WriteLine( "Adı: {0}, Yaşı: {1}", k.Ad, k.Yas);
+            Console.WriteLine("Program bitti.");
+        }
+    }
+}
